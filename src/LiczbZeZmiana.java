0fde878812c23ffc5ef4c8466c5886e33b80988d
wojga476 <wojga476@gmail.com>
2018-06-07 18:09:08 +0200

    wyświetlenie liczb ze zmianą 0.1

---
+++ src/LiczbZeZmiana.java
@@ -0,0 +1,15 @@
+public class LiczbZeZmiana {
+
+    public static void main(String[] args) {
+
+        double liczba =0;
+//        while(liczba<3){
+//        liczba += 0.1;
+//        System.out.println(liczba);
+//        }
+        do{
+            System.out.println(liczba);
+            liczba += 0.1;
+        }while(liczba<3);
+    }
+}
