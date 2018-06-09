0af357831cfe3095d8d9e01850d12279a778d663
wojga476 <wojga476@gmail.com>
2018-06-09 20:17:24 +0200

    poprawa-wyświetlenie liczb ze zmianą 0.1

--- src/LiczbZeZmiana.java
+++ src/LiczbZeZmiana.java
@@ -2,14 +2,14 @@ public class LiczbZeZmiana {
 
     public static void main(String[] args) {
 
-        double liczba =0;
-//        while(liczba<3){
-//        liczba += 0.1;
-//        System.out.println(liczba);
-//        }
-        do{
-            System.out.println(liczba);
+        double liczba = 0;
+        while (liczba < 3) {
             liczba += 0.1;
-        }while(liczba<3);
+            System.out.printf("%.1f ",liczba);
+        }
+        do {
+            System.out.printf("%.1f ",liczba);
+            liczba += 0.1;
+        } while (liczba < 3);
     }
 }
